12e2a53780e5875c093a0ad4d0122350fb25fa4d
Keira Goolsby <keiralynng@gmail.com>
2026-03-06 21:47:16 -0500

    Initial frontend setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import userIcon from "./images/user.svg";
+import inboxIcon from "./images/inbox.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="header">
+        <div className="logo">logo</div>
+
+        <div className="nav-pill">
+          <button className="nav-btn">Dashboard</button>
+          <button className="nav-btn">Coach</button>
+          <button className="nav-btn">Workouts</button>
+          <button className="nav-btn">Logs</button>
+          <button className="nav-btn">Calendar</button>
+          <button className="nav-btn">Payments</button>
+        </div>
+
+        <div className="right-btns">
+          <div className="icon-btn">
+            <div className="circle">
+              <img src={inboxIcon} alt="Messages icon" className="circle-icon" />
+            </div>
+            <div className="nav-small">
+              <button className="nav-btn-small">Messages</button>
+            </div>
+          </div>
+
+          <div className="icon-btn">
+            <div className="circle">
+              <img src={userIcon} alt="Profile icon" className="circle-icon" />
+            </div>
+            <div className="nav-small">
+              <button className="nav-btn-small">Profile</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
