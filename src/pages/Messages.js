61a7ea98c841b421410dc1219b0f1b2904988429
Keira Goolsby <keiralynng@gmail.com>
2026-03-19 22:53:08 -0400

    fixed dash and started messages page

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "../styles/Messages.css"; 
 
 function Profile() {
-  return <h1 className="page-title">Messages</h1>;
+  return (
+  <div className="messages-page"> 
+    <div className="messages-sidebar"></div>
+    <div className="messages-chat"> </div>
+    </div>
+  ); 
 }
 
 export default Profile;
