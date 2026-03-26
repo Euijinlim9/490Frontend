dc8bc90b21b166f4ebc5dd720ee49e5019445bdb
Keira Goolsby <keiralynng@gmail.com>
2026-03-26 17:30:50 -0400

    added survey

---
+++ src/components/Survey.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import "../styles/Survey.css";
+
+function Survey({ show, onClose }){
+    const [ form, setForm ] = useState({
+        goal: "", 
+        typeWorkout: "", 
+        dietPreference: "", 
+        currentActivity: "",
+        coachHelp: "", 
+        nutritionistHelp: "", 
+        workoutDay: "",
+    }); 
+
+    const handleChange = (e) => {
+        setForm({
+            ...form, 
+            [e.target.name]: e.target.value, 
+        });
+    }; 
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("Survey submitted!", form); 
+        //backend 
+        onClose(); 
+    }; 
+
+    if (!show) return null;
+
+    return(
+        <div className="survey-overlay">
+            <div className="survey-box"> 
+                <h2>Initial Survey</h2>
+                <form onSubmit={handleSubmit} className="survey-form"> 
+
+                    <select name="goal" value={form.goal} onChange={handleChange}>
+                        <option value="">Select Your Goal</option>
+                        <option value="none">Lose Weight</option>
+                        <option value="vegetarian">Gain Weight</option>
+                        <option value="gluten-free">Maintain</option> 
+                    </select>
+
+                    <select name="typeWorkout" value={form.typeWorkout} onChange={handleChange}>
+                        <option value="">Select Type of Workout</option>
+                        <option value="strength-training">Strength Training</option>
+                        <option value="cardio">Cardio</option>
+                        <option value="low-impact">Low Impact</option>
+                        <option value="mixed">Mixed</option>
+                    </select>
+
+                    <select name="dietPreference" value={form.dietPreference} onChange={handleChange}>
+                        <option value="">Select Diet Preferences</option>
+                        <option value="none">none</option>
+                        <option value="vegetarian">Vegetarian</option>
+                        <option value="gluten-free">Gluten Free</option>
+                        <option value="vegan">Vegan</option>
+                    </select>
+
+                    <select name="currentActivity" value={form.currentActivity} onChange={handleChange}>
+                        <option value="">Select Activity Level</option>
+                        <option value="beginner">Beginner</option>
+                        <option value="moderate">Moderate</option>
+                        <option value="active">Active</option>
+                        <option value="none">No Activity</option>
+                    </select>
+
+                    <select name="coachHelp" value={form.coachHelp} onChange={handleChange}>
+                        <option value="">Select Coaching Option</option>
+                        <option value="workout-plans">Workout Plans</option>
+                        <option value="no-help">No Coach</option>
+                    </select>
+
+                    <select name="nutritionistHelp" value={form.nutritionistHelp} onChange={handleChange}>
+                        <option value="">Select Nutritionist Option</option>
+                        <option value="meal-plans">Meal Plans</option>
+                        <option value="calorie-tracking">Calorie Tracking</option>
+                        <option value="diet-guidance">Diet Guidance</option>
+                        <option value="no-help">No Nutritionist</option>
+                    </select> 
+
+                    <select name="workoutDay" value={form.workoutDay} onChange={handleChange}>
+                        <option value="">Select Days</option>
+                        <option value="1">1 days</option>
+                        <option value="2">2 days</option>
+                        <option value="3">3 days</option>
+                        <option value="4">4 days</option>
+                        <option value="5">5 days</option>
+                        <option value="6">6 days</option>
+                    </select> 
+
+                    <button type="submit" className="submit-btn">Submit</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Survey;
