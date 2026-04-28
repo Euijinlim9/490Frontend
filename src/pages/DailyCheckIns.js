f005e47bffd194df318a48661e19f0fd12fe86a5
Keira Goolsby <keiralynng@gmail.com>
2026-04-28 16:16:28 -0400

    import navigate from wrong place fix

--- src/pages/DailyCheckIns.js
+++ src/pages/DailyCheckIns.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"; 
-import { useNavigate } from "react";
 import "../styles/CheckIn.css"; 
+import { useNavigate } from "react-router-dom"; 
 
 function DailyCheckIns(){
     const [formData, setFormData] = useState({
