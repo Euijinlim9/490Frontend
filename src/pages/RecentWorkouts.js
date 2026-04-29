69e51a1dd02323ac4bc59831cea4b84dceb34be4
Keira Goolsby <keiralynng@gmail.com>
2026-04-29 12:48:44 -0400

    fix cardio log

--- src/pages/RecentWorkouts.js
+++ src/pages/RecentWorkouts.js
@@ -7,14 +7,22 @@ function RecentWorkouts() {
   useEffect(() => {
     const savedWorkouts =
       JSON.parse(localStorage.getItem("loggedWorkouts")) || [];
+
     setWorkouts([...savedWorkouts].reverse());
   }, []);
 
   const handleDeleteWorkout = (indexToDelete) => {
-    const updatedWorkouts = workouts.filter((_, index) => index !== indexToDelete); 
-    setWorkouts(updatedWorkouts); 
-    localStorage.setItem("loggedWorkouts", JSON.stringify([...updatedWorkouts].reverse())); 
-  }; 
+    const updatedWorkouts = workouts.filter(
+      (_, index) => index !== indexToDelete
+    );
+
+    setWorkouts(updatedWorkouts);
+
+    localStorage.setItem(
+      "loggedWorkouts",
+      JSON.stringify([...updatedWorkouts].reverse())
+    );
+  };
 
   return (
     <div className="recent-workouts-page">
@@ -34,23 +42,50 @@ function RecentWorkouts() {
               >
                 Delete
               </button>
-              
+
               <div className="recent-workout-content">
-                <div className="recent-workout-name">{workout.workoutType}</div>
+                <div className="recent-workout-name">
+                  {workout.workoutType || "Workout"}
+                </div>
 
                 <div className="recent-workout-minutes">
-                  {workout.duration} Minutes
+                  {workout.duration || workout.time || 0} Minutes
                 </div>
 
                 <div className="recent-workout-details">
-                  <span className="recent-workout-sets">
-                    {workout.sets} Sets
-                  </span>
-                  <span className="recent-workout-divider">|</span>
-                  <span className="recent-workout-reps">
-                    {workout.reps} Reps
-                  </span>
+                  {workout.sets && <span>{workout.sets} Sets</span>}
+
+                  {workout.reps && (
+                    <>
+                      {workout.sets && (
+                        <span className="recent-workout-divider">|</span>
+                      )}
+                      <span>{workout.reps} Reps</span>
+                    </>
+                  )}
+
+                  {workout.incline && (
+                      <span>Incline: {workout.incline}</span>
+                  )}
+
+                  {workout.speed && (
+                    <>
+                      <span className="recent-workout-divider">|</span>
+                      <span>Speed: {workout.speed} mph</span>
+                    </>
+                  )}
+
+                  {workout.distance && (
+                    <>
+                      <span className="recent-workout-divider">|</span>
+                      <span>Distance: {workout.distance} mi</span>
+                    </>
+                  )}
                 </div>
+
+                {workout.date && (
+                  <div className="recent-workout-date">{workout.date}</div>
+                )}
               </div>
             </div>
           ))
